852abc6909966bb077ff4b2b3e4b18f955fe2abd
Muhammad Ali Akbar <maakbarofficial@gmail.com>
2026-08-27 19:32:30 +0500

    UI/UX fixes

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import './unit-ui.css'
 import './food-loader.css'
+import './mobile-fixes.css'
 import App from './AppV2.jsx'
 
 createRoot(document.getElementById('root')).render(
